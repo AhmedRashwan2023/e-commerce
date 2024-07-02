2b70eeea810d3ce8cb69d2219db84bb03156e5a2
AhmedRashwan2023 <rash.ahmed.2023@gmail.com>
2024-07-02 22:19:59 +0300

    ...

--- src/app/[local]/shopping-cart/action.ts
+++ src/app/[local]/shopping-cart/action.ts
@@ -1,10 +1,14 @@
 "use server";
 
+import { getSession } from "@/services/auth";
+
 export const executeCart = async (formData: FormData, cartItems: any) => {
+  const session = await getSession();
   const order = {
     items: cartItems,
     addressId: formData.get("address"),
     payment: formData.get("payment"),
+    user_id: session?.data?.id,
   };
   console.log(order);
 };
